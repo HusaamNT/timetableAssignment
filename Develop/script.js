abc64423b4bc2b577756147275bc2577a8a687bf
Husaam NT <husnaeem@gmail.com>
2022-10-31 13:35:34 +0000

    Begun adding inputs and moments variable

---
+++ Develop/script.js
@@ -0,0 +1,8 @@
+const timeBlockEl = $(".timeBlocks")
+const timeBlockInputs = $(".timeBlockInputs")
+const timeBlockSave = $(".timeBlockSave")
+
+
+/*var timeDay = today.format("[Today is] dddd")
+$("timeDay").text(timeDay);
+*/
