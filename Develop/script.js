b3b333de3c1b791aa9834305757a1e2181eae17f
Husaam NT <husnaeem@gmail.com>
2022-10-31 13:54:24 +0000

    Added momentjs times

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,12 @@ const timeBlockEl = $(".timeBlocks")
 const timeBlockInputs = $(".timeBlockInputs")
 const timeBlockSave = $(".timeBlockSave")
 
+let today = moment();
+$("#currentDate").text(today.format("MMM Do, YYYY"));
 
-/*var timeDay = today.format("[Today is] dddd")
-$("timeDay").text(timeDay);
-*/
+let todayDate = today.format("[Today is] dddd")
+$("#currentDay").text(todayDate);
+
+let todayTime = today.format("[Current time: ] hh:mm:ss A")
+$("#currentTime").text(todayTime);
+
