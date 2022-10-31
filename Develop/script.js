feb2ed0d93a575fc8cfa13e70d5d9ad472b05273
Husaam NT <husnaeem@gmail.com>
2022-10-31 15:49:55 +0000

    Added all input block consts

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,32 @@ const timeBlockEl = $(".timeBlocks")
 const timeBlockInputs = $(".timeBlockInputs")
 const timeBlockSave = $(".timeBlockSave")
 
+const time9am = $("#9am")
+const time10am = $("#9am")
+const time11am = $("#9am")
+const time12pm = $("#12pm")
+const time1pm = $("#1pm")
+const time2pm = $("#2pm")
+const time3pm = $("#3pm")
+const time4pm = $("#4pm")
+const time5pm = $("#5pm")
+const time6pm = $("#6pm")
+const time7pm = $("#7pm")
+const time8pm = $("#8pm")
+const time9pm = $("#9pm")
+const time10pm = $("#10pm")
+const time11pm = $("#11pm")
+const time12am = $("#12am")
+const time1am = $("#1am")
+const time2am = $("#2am")
+const time3am = $("#3am")
+const time4am = $("#4am")
+const time5am = $("#5am")
+const time6am = $("#6am")
+const time7am = $("#7am")
+
+
+
 let today = moment();
 $("#currentDate").text(today.format("MMM Do, YYYY"));
 
@@ -10,4 +36,6 @@ $("#currentDay").text(todayDate);
 
 let todayTime = today.format("[Current time: ] hh:mm:ss A")
 $("#currentTime").text(todayTime);
+
+
 
